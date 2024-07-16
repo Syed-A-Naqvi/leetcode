81a10c0a5cd7671e28372d35a1a56f98cef12708
Syed Arham Naqvi <syedm.naqvi@ontariotechu.net>
2024-07-16 13:30:12 -0400

    finding the bottom left node of a binary tree using reverse level-order traversal

---
+++ medium/bottomLeftTreeNode/main.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <queue>
+
+
+struct TreeNode
+{
+    // fields
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    // constructors
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* right) : val(x), left(nullptr), right(right) {}
+    TreeNode(TreeNode* left, int x) : val(x), left(left), right(nullptr) {}
+    TreeNode(TreeNode* left, int x, TreeNode* right) : val(x), left(left), right(right) {}
+    TreeNode(TreeNode* left) : val(0), left(left), right(nullptr) {}
+    TreeNode(TreeNode* right) : val(0), left(nullptr), right(right) {}    
+
+};
+
+class Solution
+{
+    public:
+        int findBottomLeftValue(TreeNode* root)
+        {
+            std::queue<TreeNode*> nodeQ;
+            nodeQ.push(root);
+
+            TreeNode* node;
+            while(!nodeQ.empty())
+            {
+                node = nodeQ.front();
+                nodeQ.pop();
+
+                if(node->right) { nodeQ.push(node->right);}
+                if(node->left) { nodeQ.push(node->left);}
+            }
+
+            return node->val;
+        }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
