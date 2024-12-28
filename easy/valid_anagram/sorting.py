3a1cb7bda69e01510f9412d5ec6602272c91105d
Syed Arham Naqvi <syedm.naqvi@ontariotechu.net>
2024-12-27 20:22:26 -0500

    valid anagram solution understood

---
+++ easy/valid_anagram/sorting.py
@@ -0,0 +1,14 @@
+# Sorting then comparing
+    # if the strings are different lengths return false immediately
+    # else sort both strings
+    # iterate through both strings at once, as soon as a mismatch is encountered return false
+    # return true if all characters are the same
+        # may just be able to compare the strings directly (i.e. string1 == string2)
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         s.sort()
+
+s = "word"
+print(s)
+print(s.sort())
