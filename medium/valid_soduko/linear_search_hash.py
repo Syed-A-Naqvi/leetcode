3b02f2082b1a2cfce80315dfc5f87a2bd5c8a396
syed arham naqvi <syedm.naqvi@ontariotechu.net>
2025-01-04 16:41:33 -0500

    valid sudoku board practice

---
+++ medium/valid_soduko/linear_search_hash.py
@@ -0,0 +1,41 @@
+from typing import List, Union
+
+class Solution:
+    
+    def isValidSudoku(self, board: List[List[Union[str,int]]]) -> bool: 
+        
+        for r in board:
+            seen = set()
+            for v in r:
+                if v == '.':
+                    continue
+                if(v in seen):
+                    return False
+                else:
+                    seen.add(v)
+        
+        for c in range(len(board[0])):
+            seen = set()
+            for r in range(len(board)):
+                if board[r][c] == '.':
+                    continue
+                if board[r][c] in seen:
+                    return False
+                else:
+                    seen.add(board[r][c])
+        
+        for square in range(9):
+            
+
+
+board = [["1","2",".",".","3",".",".",".","."],
+         ["4",".",".","5",".",".",".",".","."],
+         [".","9","8",".",".",".",".",".","3"],
+         ["5",".",".",".","6",".",".",".","4"],
+         [".",".",".","8",".","3",".",".","5"],
+         ["7",".",".",".","2",".",".",".","6"],
+         [".",".",".",".",".",".","2",".","."],
+         [".",".",".","4","1","9",".",".","8"],
+         [".",".",".",".","8",".",".","7","9"]]
+
+print(Solution().isValidSudoku(board))
