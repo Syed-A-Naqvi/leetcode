b597b0773409a2cf1ff6ccc998fb893d437d6d59
syed arham naqvi <syedm.naqvi@ontariotechu.net>
2025-01-13 11:57:00 -0500

    still need to complete documentation for valid_parenthesis solution

--- stacks/valid_parenthesis/stacking_brackets.js
+++ stacks/valid_parenthesis/stacking_brackets.js
@@ -3,10 +3,49 @@ class Solution {
      * @param {string} s
      * @return {boolean}
      */
-    isValid(s) {
+    isValid(s){
+
         let par_stack = []
+        let openBrackets = new Set(['(', '[', '{']);
+        let closeBrackets = new Set([')', ']', '}']);
         
-        for c in s:
+        if (s == ''){
+            return true;
+        }
+
+        for (let c of s){
+
+            if (openBrackets.has(c)){             
+                par_stack.push(c);
+            }
+            else if (closeBrackets.has(c)){
+                let open = par_stack.pop();
+                if (!open){
+                    return false; 
+                } else if(open == '(' && c != ')'){
+                    print();
+                    return false;
+                } else if(open == '[' && c != ']'){
+                    print();
+                    return false;
+                } else if(open == '{' && c != '}'){
+                    print();
+                    return false;
+                }
+            }
+        }
         
+        if (par_stack.length){
+            return false;
+        }
+        else {
+            return true;
+        }
+
     }
 }
+
+let parenthesis_check = new Solution();
+let s = "([{}])"
+
+console.log(parenthesis_check.isValid(s));
